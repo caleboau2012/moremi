3e9ebe0439ca6c2068c01638b70094eb209c3b0c
i_am_invisible <samuel.james@venturegardengroup.com>
2017-02-05 21:39:56 +0100

    Implemented feature to save images from urls and base64 strings

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,3 +27,4 @@ Route::get('cheeks/{total}', array("as" => "cheeks", "uses" => 'HomeController@g
 Route::get('cron_post', array("as" => 'cron_post', 'uses' => 'FacebookController@post'));
 Route::get('facebook_redirect', ["as" => "facebook_redirect", "uses" => 'FacebookController@login']);
 Route::post('update/status','PhotoController@updateStatus');
+Route::post('upload/photo','PhotoController@storeImgFromString');
---
+++ app/Traits/ProcessImageTrait.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Samuel.James
+ * Date: 2/5/2017
+ * Time: 2:51 PM
+ */
+
+namespace app\Traits;
+
+
+use Intervention\Image\Facades\Image;
+
+trait ProcessImageTrait
+{
+
+
+
+}
