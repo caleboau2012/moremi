d70b0f8985845ac87a40f561c876a37634046c56
moses adebayo <mos.adebayo@gmail.com>
2017-04-12 01:02:31 +0100

    vote reset and the winners saved

--- database/migrations/2017_04_11_224943_make_voter_table_soft_delete.php
+++ database/migrations/2017_04_11_224943_make_voter_table_soft_delete.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddHighestVoterOldCheek extends Migration
+class MakeVoterTableSoftDelete extends Migration
 {
     /**
      * Run the migrations.
@@ -12,9 +12,10 @@ class AddHighestVoterOldCheek extends Migration
      */
     public function up()
     {
-        Schema::table('old_cheeks', function (Blueprint $table) {
+        //
+        Schema::table('voters', function (Blueprint $table) {
             //
-            $table->integer('voter_id')->after('user_id');
+            $table->timestamp('deleted_at')->nullable();
         });
     }
 
@@ -25,9 +26,10 @@ class AddHighestVoterOldCheek extends Migration
      */
     public function down()
     {
-        Schema::table('old_cheeks', function (Blueprint $table) {
+
+        Schema::table('voters', function (Blueprint $table) {
             //
-            $table->removeColumn('voter_id');
+            $table->dropColumn('deleted_at');
         });
     }
 }
