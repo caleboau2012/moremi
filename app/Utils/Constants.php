9b2dba018c9f3cdadd6c52756e35daad23670910
moses adebayo <mos.adebayo@gmail.com>
2017-04-17 23:42:44 +0100

    send mail to winner, highest voter and admin

---
+++ app/Utils/Constants.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moscoworld
+ * Date: 4/11/17
+ * Time: 11:17 PM
+ */
+class MailConstants{
+    const TEAM_MAIL = 'mos.adebayo@gmail.com';
+    const TEAM_NAME = 'Moree.me Team';
+    const SUPPORT_MAIL = 'support@moree.me';
+
+}
