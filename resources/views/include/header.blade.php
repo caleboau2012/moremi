c576d7f8b7673283c253e4114b2c52b82ae9e84e
moses adebayo <mos.adebayo@gmail.com>
2017-05-06 02:41:41 +0100

    Landing UI implementation of the mock up

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,7 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: moscoworld
- * Date: 4/22/17
- * Time: 9:57 AM
- */
+
+<nav class="navbar hidden" id="landing_page_nav_bar">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mooree-navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Moree.me</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="mooree-navbar-collapse">
+
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">About</a></li>
+        <li><a href="#">Trending</a></li>
+        <li><a href="#">Join</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
--- resources/views/uirevamp/home.blade.php
+++ resources/views/uirevamp/home.blade.php
@@ -9,71 +9,125 @@
             <div class="content">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-6" data-scrollreveal="enter left after 0.15s over 1s">
-                            <h2 class="text-white">
-                                It all begins with a
-                                <br>
-                                <span class="bg-text text-primary">DATE!</span>
-                            </h2>
+                        <div class="col-md-10 col-md-offset-1" data-scrollreveal="enter left after 0.15s over 1s">
+                            <h1 class="text-white text-center text-uppercase">
+                                <strong>
+                                    It all begins with a Date
+                                </strong>
+                            </h1>
+                            <div class="row">
+                                <p class="text-center text-white col-md-6 col-md-offset-3">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque eligendi impedit laudantium pariatur provident velit.
+                                </p>
+                            </div>
+                            <div class="row text-center">
+                                <button class="btn btn-default get_started">Get Started</button>
+                            </div>
                         </div>
 
-                        <div class="col-md-5 col-md-offset-1" data-scrollreveal="enter right after 0.15s over 1s">
-                           <div id="login_container">
-                                <h3 class="no-margin text-white">Starts Here</h3>
-                               <p class="text-grey">
-                                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci deleniti, dignissimos dolore error incidunt nam?
-                               </p>
-                               <div>
-                                   <button class="btn btn-lg btn-brand-primary btn-primary btn-fill">Get Started</button>
-                               </div>
-                           </div>
-                        </div><!--Col-md-6-->
                     </div><!-- Row -->
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="container">
+    {{--About Moore.me--}}
+    <div id="about_content">
         <div class="row">
-            <div class="col-md-3">
-                <ul class="roundabout roundabout-holder" style="padding: 0; position: relative;">
-                    <li class="roundabout-slide"><img class="img-thumbnail img-responsive" src="{{ asset('images/cheeks/0.jpg') }}" alt="Cheek of the moment"></li>
-                    <li class="roundabout-slide"><img class="img-thumbnail img-responsive" src="{{ asset('images/cheeks/6.jpg') }}" alt="Cheek of the moment"></li>
-                    <li class="roundabout-slide"><img class="img-thumbnail img-responsive" src="{{ asset('images/cheeks/1.jpg') }}" alt="Cheek of the moment"></li>
-                    <li class="roundabout-slide"><img class="img-thumbnail img-responsive" src="{{ asset('images/cheeks/2.jpg') }}" alt="Cheek of the moment"></li>
-                    <li class="roundabout-slide"><img class="img-thumbnail img-responsive" src="{{ asset('images/cheeks/3.jpg') }}" alt="Cheek of the moment"></li>
-                    <li class="roundabout-slide"><img class="img-thumbnail img-responsive" src="{{ asset('images/cheeks/7.jpg') }}" alt="Cheek of the moment"></li>
-                </ul>
+            <div class="col-md-6">
+                <img src="{{asset('images/about_banner.jpg')}}" alt="" class="img-responsive">
             </div>
-            
-            <div class="col-md-5 col-md-offset-2">
-                <h3>Cheek Of The Moment</h3>
-                <hr class="no-margin-top">
-                <h4 class="text-primary">
-                    Adedoyin Omotayo
-                </h4>
-                <p>
-                    <span class="icon icon-location">&nbsp;</span>Lagos, Nigeria
-                </p>
-                <p>
-                    <strong>Bio:</strong> <br>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores quibusdam sequi voluptate. Asperiores, laborum, sint.
-                </p>
-                <h5>
-                    <a href="#"><span class="icon icon-instagram text-instagram"></span></a>
-                    <a href="#"><span class="icon icon-facebook-official text-facebook"></span></a>
-                    <a href="#"><span class="icon icon-twitter text-twitter"></span></a>
-                </h5>
+            <div class="col-md-4 col-md-offset-1">
+                <div id="about_caption_content">
+                    <h4 class="text-primary">Get Connected on Moree.me</h4>
+                    <p>
+                        Moore.me is a best application that brings out the best in people. It's easy to use
+                    </p>
+                    <ul>
+                        <li>
+                            Login with your facebook account
+                        </li>
+                        <li>
+                            Create a profile
+                        </li>
+                        <li>
+                            Start voting or accumulate vote to get to your office
+                        </li>
+                    </ul>
+                    <div>
+                        <button class="btn get_started">Get Started</button>
+                    </div>
+                </div>
+
             </div>
-            
+
         </div>
+    </div>
+
+    {{--PICK OF THE MOMENT--}}
+    <div id="moment_pick_container" class="center-block">
+        <div class="container">
+            <div class="row">
+                <h3 id="header" class="text-center text-white">Pick of the Moment</h3>
+            </div>
+            <div class="row moment_pick_container_content" style="position:relative;">
+                <div class="moment_pics_container pull-left">
+                    <ul class="roundabout roundabout-holder" style="padding: 0; position: relative;">
+                        <li class="roundabout-slide"><img class="img-thumbnail img-responsive" src="{{ asset('images/cheeks/0.jpg') }}" alt="Cheek of the moment"></li>
+                        <li class="roundabout-slide"><img class="img-thumbnail img-responsive" src="{{ asset('images/cheeks/6.jpg') }}" alt="Cheek of the moment"></li>
+                        <li class="roundabout-slide"><img class="img-thumbnail img-responsive" src="{{ asset('images/cheeks/1.jpg') }}" alt="Cheek of the moment"></li>
+                        <li class="roundabout-slide"><img class="img-thumbnail img-responsive" src="{{ asset('images/cheeks/2.jpg') }}" alt="Cheek of the moment"></li>
+                        <li class="roundabout-slide"><img class="img-thumbnail img-responsive" src="{{ asset('images/cheeks/3.jpg') }}" alt="Cheek of the moment"></li>
+                        <li class="roundabout-slide"><img class="img-thumbnail img-responsive" src="{{ asset('images/cheeks/7.jpg') }}" alt="Cheek of the moment"></li>
+                    </ul>
+                </div>
+                <div class="pull-left moment_profile_container">
+                    <div class="content name_content">
+                        <h4 class="no-margin-bottom name">Chioma Nwakezuologoomigwojere</h4>
+                        <span class="content-end"></span>
+
+                    </div>
+                    <p class="content no-margin-bottom">
+                        <span class="icon icon-location text-primary">&nbsp;</span>Lagos, Nigeria
+                    </p>
+                    <p class="content no-margin-top">
+                        <span class="icon icon-heart3 text-primary">&nbsp;</span>5,000
+                    </p>
+                    <p class="content">
+                        <strong>Status:</strong> <br>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores quibusdam sequi voluptate.
+                    </p>
+                    <h5 class="content">
+                        <a href="#"><span class="icon icon-instagram text-primary"></span></a>
+                        <a href="#"><span class="icon icon-facebook-official text-primary"></span></a>
+                        <a href="#"><span class="icon icon-twitter text-primary"></span></a>
+                    </h5>
+                </div>
+
+                <div class="moment_pick_voter_container">
+                    <img src="{{asset('images/users/moses.jpg')}}" alt="" width="65" class="img-circle">
+                    <div class="description">
+                        <h5 class="no-margin">Highest Picker</h5>
+                        <p class="no-margin text-white">Adamu Musa</p>
+                        <p class="no-margin text-white">Lagos, Nigeria</p>
+                    </div>
+                </div>
+
+            </div>
+    </div>
 
     </div>
 
-    <div class="container-fluid bg-grey" >
+    <div class="container-fluid bg-grey" id="trending_container" >
         <div class="container">
-            <h3 class="text-center margin-top-md">TRENDING</h3>
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <div class="btn-group btn-group-justified">
+                        <a href="#" class="btn active  trending_menu">Trending</a>
+                        <a href="#" class="btn  trending_menu">Vote Your Pick</a>
+                    </div>
+                </div>
+            </div>
             <div class="row trending-items">
                 <div class="trending-item">
                     <div class="profile-card">
@@ -92,7 +146,7 @@
                                 <span class="icon icon-heart3">&nbsp;</span>5,000
                             </p>
                             <div class="text-center">
-                                <button class="btn btn-primary btn-sm vote-btn btn-fill">Vote</button>
+                                <button class="btn get_started btn-sm vote-btn btn-fill">Vote</button>
                             </div>
                         </div>
 
@@ -116,7 +170,7 @@
                                 <span class="icon icon-heart3">&nbsp;</span>1,000
                             </p>
                             <div class="text-center">
-                                <button class="btn btn-primary btn-sm vote-btn btn-fill">Vote</button>
+                                <button class="btn get_started btn-sm vote-btn btn-fill">Vote</button>
                             </div>
                         </div>
 
@@ -139,7 +193,7 @@
                                 <span class="icon icon-heart3">&nbsp;</span>80
                             </p>
                             <div class="text-center">
-                                <button class="btn btn-primary btn-sm vote-btn btn-fill">Vote</button>
+                                <button class="btn get_started btn-sm vote-btn btn-fill">Vote</button>
                             </div>
                         </div>
 
@@ -163,7 +217,7 @@
                                 <span class="icon icon-heart3">&nbsp;</span>300
                             </p>
                             <div class="text-center">
-                                <button class="btn btn-primary btn-sm vote-btn btn-fill">Vote</button>
+                                <button class="btn get_started btn-sm vote-btn btn-fill">Vote</button>
                             </div>
                         </div>
 
@@ -187,7 +241,7 @@
                                 <span class="icon icon-heart3">&nbsp;</span>1,000
                             </p>
                             <div class="text-center">
-                                <button class="btn btn-primary btn-sm vote-btn btn-fill">Vote</button>
+                                <button class="btn get_started btn-sm vote-btn btn-fill">Vote</button>
                             </div>
                         </div>
 
@@ -211,7 +265,7 @@
                                 <span class="icon icon-heart3">&nbsp;</span>48,000
                             </p>
                             <div class="text-center">
-                                <button class="btn btn-primary btn-sm vote-btn btn-fill">Vote</button>
+                                <button class="btn get_started btn-sm vote-btn btn-fill">Vote</button>
                             </div>
                         </div>
 
@@ -235,7 +289,7 @@
                                 <span class="icon icon-heart3">&nbsp;</span>39,010
                             </p>
                             <div class="text-center">
-                                <button class="btn btn-primary btn-sm vote-btn btn-fill">Vote</button>
+                                <button class="btn get_started btn-sm vote-btn btn-fill">Vote</button>
                             </div>
                         </div>
 
@@ -249,11 +303,42 @@
     <div id="profile-counter-container">
         <div class="overlay">
             <div class="container">
-                <h2 class="text-center text-white">Over 20,0000 Cheeks</h2>
-                <div class="text-center">
-                    <a href="#" class="btn btn-primary btn-lg">Connect Now</a>
+                <div class="col-md-4">
+                    <div class="text-center">
+                        <span class="text-white icon icon-profile-female"></span>
+                        <h3 class="no-margin text-center text-white">2000+</h3>
+                        <div class="divider"></div>
+                        <h5 class="no-margin text-white text-center">Registered Ladies</h5>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="text-center">
+                        <span class="text-white icon icon-profile-male"></span>
+                        <h3 class="no-margin text-center text-white">2000+</h3>
+                        <div class="divider"></div>
+                        <h5 class="no-margin text-white text-center">Registered Men</h5>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="text-center">
+                        <span class="text-white icon icon-tags"></span>
+                        <h3 class="no-margin text-center text-white">2000+</h3>
+                        <div class="divider"></div>
+                        <h5 class="no-margin text-white text-center">Sponsored Dates</h5>
+                    </div>
+                </div>
+                <div class="text-center margin-top-sm">
+                    <a href="#" class="btn get_started btn-lg">Connect Now</a>
                 </div>
             </div>
+        </div>
+    </div>
+
+    {{--PARTNERS--}}
+    <div class="container" id="partners_section">
+        <h3 class="text-center">Partners</h3>
+        <div class="row">
+
         </div>
     </div>
 @endsection
