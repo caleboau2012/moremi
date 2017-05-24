265374c822af5abb0e9e926714799aa14da8a6df
moses adebayo <mos.adebayo@gmail.com>
2017-05-24 07:42:17 +0100

    positioning in app header

--- resources/views/include/header_app.blade.php
+++ resources/views/include/header_app.blade.php
@@ -2,7 +2,37 @@
  <div class="container-fluid">
    <img id="logo" src="{{asset('images/logo.png')}}" alt="">
  </div>
-  <div><a href="{{route("profile")}}">
+    <div class="container">
+        <div id="profile_cover_info_container">
+            <div class="profile_info_item">
+                <p class="profile_info_item text-center text-white no-margin">Pick of the Week</p>
+                <h4 class="no-margin text-white text-center">
+                    <strong class="icon icon-heart3 text-white">&nbsp;</strong>20
+                </h4>
+
+            </div>
+            <div class="profile_info_item" id="profile_dp_container">
+                @if($profile->photo()->first())
+                    <img id="profile-cover_dp"  src="{{asset($profile->photo()->first()->full_path)}}" alt="Profile DP">
+                @else
+                    <img src="{{asset('images/apple-icon.png')}}" id="profile-cover_dp">
+                @endif
+                    <a href="#" class="dp-edit-btn"><span class="icon icon-pencil2 text-white"></span></a>
+            </div>
+
+            <div class="profile_info_item text-left">
+                <h4 class="profile_info_item text-white no-margin"> {{$profile->first_name}} {{$profile->last_name}} </h4>
+                @if($profile->about)
+                    <p class="no-margin text-white"><strong>Status:</strong></p>
+                    <p class="no-margin text-white">{{$profile->about}}</p>
+                @else
+                 <br><br>
+                @endif
+            </div>
+        </div>
+    </div>
+  <div class="hidden">
+      <a href="{{route("profile")}}">
     <div id="profile-container hidden-xs hidden-sm">
       <div id="profile-info-left" class="profile-info hidden-sm hidden-xs">
         <h4 class="no-margin text-white">{{$profile->first_name}} {{$profile->last_name}}</h4>
@@ -34,7 +64,7 @@
   </a></div>
 </div>
 
-<div class="visible-xs visible-sm" id="app-header">
+<div class="hidden visible-xs visible-sm" id="app-header">
   <div class="container-fluid">
     <img id="logo" src="{{asset('images/logo.png')}}" alt="">
   </div>
@@ -44,12 +74,12 @@
         @if($profile->photo()->first())
           <img src="{{asset($profile->photo()->first()->full_path)}}" alt="Profile DP" class="img-responsive img-circle">
         @else
-          <img src="{{asset('images/apple-icon.png')}}" class="profile-thumb">
+              <img class="profile_info_item" src="{{asset('images/apple-icon.png')}}" alt="" style="width: 100px; height: 100px; border-radius: 50%;">
         @endif
       </div>
     </div>
     <h4 class="text-center no-margin-bottom">{{$profile->first_name}} {{$profile->last_name}}</h4>
-    <p class="no-margin">
+    <p class="no-margin text-center">
       <strong>Status: </strong> {{$profile->about}}
     </p>
     <div class="text-center">
