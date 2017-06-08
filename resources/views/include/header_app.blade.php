ff5566ac50225fb0c89b4b40582f09fa5acf3557
moses adebayo <mos.adebayo@gmail.com>
2017-06-08 01:13:41 +0100

    app header

--- resources/views/include/header_app.blade.php
+++ resources/views/include/header_app.blade.php
@@ -1,8 +1,46 @@
+<div class="st-navbar st-navbar-mini">
+    <div class="navbar navbar-default navbar-fixed-top clearfix" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sept-main-nav">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="{{route('index')}}"><img src="{{asset('images/logo3.png')}}" alt="" class="img-responsive"></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse main-nav" id="sept-main-nav">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="{{route('app')}}">Find a Date</a></li>
+                    <li><a href="#home" class="edit_profile_btn">Edit Profile</a></li>
+                    <li class="active">
+                        <a href="{{route("profile")}}" class="profile-btn">
+                            @if($profile != null)
+                                @if($profile->photo()->first())
+                                    <img src="{{asset($profile->photo()->first()->thumb_path)}}" class="profile-thumb">
+                                @else
+                                    <img src="{{asset('images/default.png')}}" class="profile-thumb">
+                                @endif
+                                <span>{{$profile->first_name}} {{$profile->last_name}}</span>
+                            @else
+                                <img src="{{asset('images/default.png')}}" class="profile-thumb">
+                            @endif
+                        </a>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div>
+    </div>
+</div>
+
+<div>
 <div id="app-header" class="hidden-xs hidden-sm">
     <div class="container-fluid">
-        <a href="{{route('index')}}">
-            <img id="logo" src="{{asset('images/logo.png')}}" alt="">
-        </a>
+       <div style="height: 70px;"></div>
     </div>
     <div class="container">
         <div class="row">
@@ -134,5 +172,5 @@
         </div>
     </div>
 </div>
-
+</div>
 @include('utils.account')
