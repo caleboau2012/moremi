5ac965e9d102ca0591c428c149f9af236b160a3d
KayLee <caleboau2012@gmail.com>
2017-07-01 22:22:35 +0100

    Added Facebook SDK to public pages to allow login.
    #kaylee

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -6,6 +6,24 @@
 
 @section('header')
     @include('include.header_public_profile')
+    <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '469144689836682',
+                xfbml      : true,
+                version    : 'v2.7'
+            });
+            FB.Event.subscribe('xfbml.render', Facebook.status);
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
 @endsection
 
 @section('content')
