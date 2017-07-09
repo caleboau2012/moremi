5cbc608185e5fcade5559f8c57d1846674231e92
moses adebayo <mos.adebayo@gmail.com>
2017-07-09 23:33:05 +0100

    style spot list options

--- resources/views/utils/meet.blade.php
+++ resources/views/utils/meet.blade.php
@@ -17,7 +17,6 @@
                             @foreach($spots as $spot)
                                 <div class="col-md-4">
                                     <div class="spot-item">
-                                        <form method="POST" action="{{ route('meet') }}" accept-charset="UTF-8" class="form-horizontal meet" role="form">
                                          <div class="card hovercard">
                                             <div class="cardheader"></div>
                                             <div class="avatar center-block">
@@ -38,23 +37,24 @@
                                                 <h4 class="no-margin text-muted line-through">&#x20a6;{{number_format($spot->price, 2)}}</h4>
 
                                             </div>
-
                                             <div class="bottom">
-                                                <input type="hidden" name="email" value="{{$profile->email}}"> {{-- required --}}
-                                                <input type="hidden" name="amount" value="{{$spot->discounted * 100}}"> {{-- required in kobo --}}
-                                                <input type="hidden" name="quantity" value="1">
-                                                <input type="hidden" name="reference" value="{{ Paystack::genTranxRef() }}"> {{-- required --}}
-                                                <input type="hidden" name="key" value="{{ config('paystack.secretKey') }}"> {{-- required --}}
-                                                <input type="hidden" name="_token" value="{{ csrf_token() }}"> {{-- employ this in place of csrf_field only in laravel 5.0 --}}
-                                                <input type="hidden" name="voted_profile_id" value="">
-                                                <input type="hidden" name="spot" value="{{$spot->id}}"> {{-- The spot ID --}}
-                                                <button class="btn main-btn btn-block" type="submit"
-                                                        data-loading-text="<i class='icon icon-circle-o-notch icon-spin'></i> Processing...">
-                                                    <img src="{{asset('images/favicon.png')}}" width="30px"> Meet Here
-                                                </button>
+                                                <form method="POST" action="{{ route('meet') }}" accept-charset="UTF-8" class="form-horizontal meet" role="form">
+                                                    <input type="hidden" name="email" value="{{$profile->email}}"> {{-- required --}}
+                                                    <input type="hidden" name="amount" value="{{$spot->discounted * 100}}"> {{-- required in kobo --}}
+                                                    <input type="hidden" name="quantity" value="1">
+                                                    <input type="hidden" name="reference" value="{{ Paystack::genTranxRef() }}"> {{-- required --}}
+                                                    <input type="hidden" name="key" value="{{ config('paystack.secretKey') }}"> {{-- required --}}
+                                                    <input type="hidden" name="_token" value="{{ csrf_token() }}"> {{-- employ this in place of csrf_field only in laravel 5.0 --}}
+                                                    <input type="hidden" name="voted_profile_id" value="">
+                                                    <input type="hidden" name="spot" value="{{$spot->id}}"> {{-- The spot ID --}}
+                                                    <button class="btn main-btn btn-block" type="submit"
+                                                            data-loading-text="<i class='icon icon-circle-o-notch icon-spin'></i> Processing...">
+                                                        <img src="{{asset('images/favicon.png')}}" width="30px"> Meet Here
+                                                    </button>
+                                                </form>
                                             </div>
                                         </div>
-                                        </form>
+
                                     </div>
                                 </div>
                             @endforeach
