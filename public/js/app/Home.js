3cce00be2217a03354898755a2cc28e7ffee1ee7
KayLee <caleboau2012@gmail.com>
2017-07-27 16:41:38 +0100

    Disabled all JS.
    #kaylee

--- public/js/app/Home.js
+++ public/js/app/Home.js
@@ -5,69 +5,69 @@
  * Time: 10:19 AM
  */
 $(function () {
-    $(window).stellar({
-        horizontalScrolling: false
-    });
+    //$(window).stellar({
+    //    horizontalScrolling: false
+    //});
 
     // Custom Scrollbar
-    var nice = $("html").niceScroll({
-        cursorwidth: 8,
-        cursorborder: "0px solid #fff",
-        cursorborderradius: '0'
-    });
-
-    $('.main-nav a:not(.dropdown-toggle)').bind('click', function(event) {
-        var $anchor = $(this);
-
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-
-        event.preventDefault();
-    });
-    /*
-     * Fun Fact with Count Animation
-     */
-    $('.st-ff-count').appear();
-    $(document.body).on('appear', '.st-ff-count', function(e, $affected) {
-        $affected.each(function(i) {
-            if (parseInt($(this).data('runit'))) {
-                $(this).countTo({
-                    speed: 3000,
-                    refreshInterval: 50
-                });
-                $(this).data('runit', "0");
-            };
-
-        });
-    });
-
-    $('[data-toggle="tooltip"]').tooltip();
-
-
-    function home_height () {
-        var element = $('.st-home-unit'),
-            elemHeight = element.height(),
-            winHeight = $(window).height()
-        var padding = (winHeight - elemHeight - 250) /2;
-
-        if (padding < 1 ) {
-            padding = 0;
-        };
-        element.css('padding', padding+'px 0');
-    }
-    home_height ();
-
-    $(window).resize(function () {
-        home_height ();
-    });
-
-
-    var fadeStart=$(window).height()/3 // 100px scroll or less will equiv to 1 opacity
-        ,fadeUntil=$(window).height() // 200px scroll or more will equiv to 0 opacity
-        ,fading = $('.st-home-unit')
-        ,fading2 = $('.hero-overlayer')
-        ;
+    //var nice = $("html").niceScroll({
+    //    cursorwidth: 8,
+    //    cursorborder: "0px solid #fff",
+    //    cursorborderradius: '0'
+    //});
+    //
+    //$('.main-nav a:not(.dropdown-toggle)').bind('click', function(event) {
+    //    var $anchor = $(this);
+    //
+    //    $('html, body').stop().animate({
+    //        scrollTop: $($anchor.attr('href')).offset().top
+    //    }, 1500, 'easeInOutExpo');
+    //
+    //    event.preventDefault();
+    //});
+    ///*
+    // * Fun Fact with Count Animation
+    // */
+    //$('.st-ff-count').appear();
+    //$(document.body).on('appear', '.st-ff-count', function(e, $affected) {
+    //    $affected.each(function(i) {
+    //        if (parseInt($(this).data('runit'))) {
+    //            $(this).countTo({
+    //                speed: 3000,
+    //                refreshInterval: 50
+    //            });
+    //            $(this).data('runit', "0");
+    //        };
+    //
+    //    });
+    //});
+    //
+    //$('[data-toggle="tooltip"]').tooltip();
+    //
+    //
+    //function home_height () {
+    //    var element = $('.st-home-unit'),
+    //        elemHeight = element.height(),
+    //        winHeight = $(window).height()
+    //    var padding = (winHeight - elemHeight - 250) /2;
+    //
+    //    if (padding < 1 ) {
+    //        padding = 0;
+    //    };
+    //    element.css('padding', padding+'px 0');
+    //}
+    //home_height ();
+    //
+    //$(window).resize(function () {
+    //    home_height ();
+    //});
+    //
+    //
+    //var fadeStart=$(window).height()/3 // 100px scroll or less will equiv to 1 opacity
+    //    ,fadeUntil=$(window).height() // 200px scroll or more will equiv to 0 opacity
+    //    ,fading = $('.st-home-unit')
+    //    ,fading2 = $('.hero-overlayer')
+    //    ;
 
     //$(window).bind('scroll', function(){
     //    var offset = $(document).scrollTop()
