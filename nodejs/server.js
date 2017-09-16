ed0e66aa368fc66042b3ed9f59e21594cf022376
KayLee <caleboau2012@gmail.com>
2017-09-16 09:45:22 +0100

    Reverted server chat to https.
    #kaylee

--- nodejs/server.js
+++ nodejs/server.js
@@ -3,13 +3,12 @@
  */
 var app = require('express')();
 var fs = require('fs');
-var privateKey  = fs.readFileSync('/var/www/html/moremi/nodejs/privkey.ln');
-var certificate = fs.readFileSync('/var/www/html/moremi/nodejs/fullchain.ln');
+var privateKey  = fs.readFileSync('/etc/letsencrypt/archive/moree.me/privkey1.pem');
+var certificate = fs.readFileSync('/etc/letsencrypt/archive/moree.me/fullchain1.pem');
 
 var credentials = {key: privateKey, cert: certificate};
 
 var server = require('https').Server(credentials, app);
-//server.globalAgent.options.rejectUnauthorized = false;
 var io = require('socket.io')(server);
 var redis = require('redis');
 
