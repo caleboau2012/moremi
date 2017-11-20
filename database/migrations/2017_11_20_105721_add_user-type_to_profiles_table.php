c08d5df1b39875225bc5d719ae4a345972598775
moses adebayo <mos.adebayo@gmail.com>
2017-11-20 12:01:22 +0100

    Role column added to profile table

---
+++ database/migrations/2017_11_20_105721_add_user-type_to_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserTypeToProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            //
+            $table->string('role', 50)->nullable()->after('sex');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            //
+            $table->dropColumn('role');
+
+        });
+    }
+}
