116b1d1a8338076011a01b5a7632491ff10579d0
moses adebayo <mos.adebayo@gmail.com>
2017-11-23 22:53:33 +0100

    Authorization middleware for admin users

--- app/Http/Middleware/AdminAuthenticate.php
+++ app/Http/Middleware/AdminAuthenticate.php
@@ -13,10 +13,9 @@ class AdminAuthenticate
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
-     * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
+    public function handle($request, Closure $next)
     {
        $this->authenticate();
 
